38d8413ce789310df121a77a4f9e845367726e5d
AkshathSB <110164805+AkshathSB@users.noreply.github.com>
2023-07-25 01:07:17 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -3,12 +3,22 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
-    constructor() ERC20("Degen", "DGN") {}
+     string public shop_items;
+
+    constructor() ERC20("Degen", "DGN") {
+        shop_items = "The following items are available in our shop: 1.Jacket, 2.Jeans, 3.Hoodies, 4.Jewellery";
+    }
 
         function mint(address to, uint256 amount) public onlyOwner {
             _mint(to, amount);
     }
+
+        function redeem(uint256 num) public {
+            assert(balanceOf(msg.sender)>= 100 && num<5&& num>0);
+            _burn(msg.sender, num*107);
+        }
 }
